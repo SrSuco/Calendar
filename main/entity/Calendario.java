38fd860d24413fa299c641ee082f0ddceb316c8b
Nasario <110493086+SrSuco@users.noreply.github.com>
2024-05-03 17:53:21 -0300

    1 commit

---
+++ main/entity/Calendario.java
@@ -0,0 +1,38 @@
+package com.poo.springjpademo.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Entity
+public class Calendario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_disciplina")
+    private Disciplina disciplina;
+
+    @ManyToOne
+    @JoinColumn(name = "id_professor")
+    private Professor professor;
+
+    @ManyToOne
+    @JoinColumn(name = "id_sala")
+    private Sala sala;
+
+    @ManyToOne
+    @JoinColumn(name = "id_dia")
+    private Dia dia;
+
+    @ManyToOne
+    @JoinColumn(name = "id_semestre_curso")
+    private SemestreCurso semestreCurso;
+}
